09de9462648ff63490e77be0f771fc7ac9d78b41
Bruno Costa <analistabrunocosta@gmail.com>
2022-06-27 20:54:30 -0300

    Iniciado testes automatizados

---
+++ App/__test__/desconto.test.js
@@ -0,0 +1,8 @@
+const calcularDesconto = require("../desconto")
+
+test("Testa se o valor é verdadeiro se for maior que 150",()=>{
+    expect(calcularDesconto(200)).toBeTruthy()// espero rceber um valor que seja verdadeiro
+})
+test("Testa se é Falso para valores menores que 150", ()=>{
+    expect(calcularDesconto(100)).toBeFalsy()
+})
